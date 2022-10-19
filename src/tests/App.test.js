c3b0504c09becf28022ccc82dede9cdb049c2692
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-19 15:09:24 -0300

    fix: lint erro

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
